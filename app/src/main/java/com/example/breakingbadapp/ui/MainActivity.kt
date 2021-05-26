a2039466d5c757245fda0d5c8c6cb3ff7f5c5f6f
German Hernandez <grosario@scottishpower.com>
2021-05-26 05:46:47 +0100

    Added navigation and CharactersListFragment.kt

--- app/src/main/java/com/example/breakingbadapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/breakingbadapp/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.breakingbadapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.breakingbadapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
